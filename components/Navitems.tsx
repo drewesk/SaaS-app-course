96501a0bbd9a9c13cfa593e8bbceb95bf43717fe
username <drewesk@gmail.com>
2025-07-21 13:45:25 -0400

    iterating through nav items

--- components/Navitems.tsx
+++ components/Navitems.tsx
@@ -1,8 +1,20 @@
+import { Link } from "lucide-react"
+
+const navItems = [
+  {label: 'Home', href: '/'},
+  {label: 'Companions', href: '/companions' },
+  {label: 'My Journey', href: 'my-journey'}
+]
+
 const Navitems = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="flex items-center gap-4">
+      {navItems.map(({label, href}) => (
+        <Link href={href} key={label}> 
+          {label}
+        </Link>
+      ))}
+    </nav>
   )
 }
 
