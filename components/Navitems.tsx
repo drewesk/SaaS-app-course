85922e2f599a51036daa2bb57fd0126d7eba5d72
username <drewesk@gmail.com>
2025-07-26 17:23:34 -0400

    feature done!

--- components/Navitems.tsx
+++ components/Navitems.tsx
@@ -1,17 +1,26 @@
+'use client'
+
+import { cn } from "@/lib/utils"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 
 const navItems = [
   {label: 'Home', href: '/'},
   {label: 'Companions', href: '/companions' },
-  {label: 'My Journey', href: 'my-journey'}
+  {label: 'My Journey', href: '/my-journey'}
 ]
 
 const Navitems = () => {
+  const pathname= usePathname();
+
   return (
     <nav className="flex items-center gap-4">
       {navItems.map(({label, href}) => (
-        <Link href={href} key={label}> 
+        <Link 
+            href={href} 
+            key={label} 
+            className={cn(pathname === href && 'text-primary font-semibold')}> 
           {label}
         </Link>
       ))}
